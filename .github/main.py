63dc1b56aac8de27f9498e1156a3963da630f2d6
Romain Thomas <romainthomasc@gmail.com>
2018-02-01 15:50:27 +0100

    WIP

--- .github/main.py
+++ .github/main.py
@@ -24,13 +24,46 @@ DEPENDENT_BUILD = os.getenv("DEPENDENT_BUILD", None)
 DEPLOY_KEY = os.getenv("LIEF_AUTOMATIC_BUILDS_KEY", None)
 DEPLOY_IV  = os.getenv("LIEF_AUTOMATIC_BUILDS_IV", None)
 
+GIT_USER  = "Dockerlief"
+GIT_EMAIL = "lief@quarkslab.com"
+
 LIEF_WEBSITE_REPO = "https://github.com/lief-project/lief-project.github.io.git"
 
 PYTHON = shutil.which("python")
 GIT = shutil.which("git")
 
-def clone_lief_website(branch="master"):
+def setup_lief_website(branch="master"):
     cmd = f"{GIT} clone --branch=master --single-branch {LIEF_WEBSITE_REPO}"
+
+    logger.debug(f"Executing: {cmd}")
+
+    kwargs = {
+        'shell':      True,
+        'cwd':        REPODIR,
+    }
+    p = subprocess.Popen(cmd, **kwargs)
+    p.wait()
+
+    if p.returncode:
+        sys.exit(1)
+
+    cmds = [
+        "chmod 700 .git",
+        f"{GIT} config user.name '{GIT_USER}'",
+        f"{GIT} config user.email '{GIT_EMAIL}'",
+        "{GIT} reset --soft HEAD~1",
+        "{GIT} ls-files -v",
+    ]
+
+    kwargs['cwd'] = REPODIR / "lief-project.github.io"
+    for cmd in cmds:
+        p = subprocess.Popen(cmd, **kwargs)
+        p.wait()
+
+        if p.returncode:
+            sys.exit(1)
+
+
 
 
 def build_doc(commit):
@@ -55,7 +88,8 @@ def build_doc(commit):
 def main(argv):
 
     if TRIGGER_ACTION == "build-doc":
-        return build_doc(TRIGGER_COMMIT)
+        build_doc(TRIGGER_COMMIT)
+        setup_lief_website()
 
     return 0
 
