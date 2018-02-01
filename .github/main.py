73297d1339d971c8a3363025768c4fc219b5ff74
Romain Thomas <romainthomasc@gmail.com>
2018-02-01 14:34:29 +0100

    WIP

--- .github/main.py
+++ .github/main.py
@@ -3,9 +3,12 @@ import sys
 import os
 import logging
 import pathlib
+import subprocess
+import shutil
 
 CURRENTDIR = pathlib.Path(__file__).parent
-sys.path.insert(0, CURRENTDIR.parent.as_posix())
+REPODIR    = CURRENTDIR.parent
+sys.path.insert(0, REPODIR.as_posix())
 
 TRIGGER_COMMIT  = os.getenv("TRIGGER_COMMIT",  None)
 TRIGGER_REPO    = os.getenv("TRIGGER_REPO",    None)
@@ -14,11 +17,30 @@ DEPENDENT_BUILD = os.getenv("DEPENDENT_BUILD", None)
 
 import dockerlief
 
+PYTHON = shutil.which("python")
+
+def build_doc(commit):
+    main_script = (REPODIR / "dockerlief" / "main.py").as_posix()
+    cmd = f"{PYTHON} {main_script} --debug build --branch={commit} lief-doc"
+    kwargs = {
+        'executable': '/bin/bash',
+        'stdout':     subprocess.STDOUT,
+        'stderr':     subprocess.STDOUT,
+        'shell':      True,
+        'cwd':        REPODIR,
+    }
+    p = subprocess.Popen(cmd, **kwargs)
+    p.wait()
+
+    if p.returncode:
+        sys.exit(1)
+
 
 def main(argv):
-    print(TRIGGER_COMMIT)
-    print(TRIGGER_REPO)
-    print(TRIGGER_ACTION)
+
+    if TRIGGER_ACTION == "build-doc":
+        return build_doc(TRIGGER_COMMIT)
+
     return 0
 
 if __name__ == "__main__":
